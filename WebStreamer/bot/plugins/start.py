1f1513efa91aaf4c6215bd398f3bcd34b5cc61ae
Anjana Madushanka <anjanama2007@gmail.com>
2021-04-17 22:59:15 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -8,13 +8,13 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @StreamBot.on_message(filters.command(['start', 'help']))
 async def start(b, m):
-    await m.reply('Hi, Send me a file to get an instant stream link.',
+    await m.reply('__Hi, First send me a telegram file then wait some times i can give direct link to your file :)__',
                   reply_markup=InlineKeyboardMarkup(
                       [
                           [
                               InlineKeyboardButton(
-                                  f'{emoji.STAR} Source {emoji.STAR}',
-                                  url='https://github.com/EverythingSuckz/TG-FileStreamBot'
+                                  f'{emoji.STAR} Channel {emoji.STAR}',
+                                  url='https://telegram.me/Harp_tech'
                               )
                           ]
                       ]
